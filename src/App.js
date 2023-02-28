77b66fe35ae5f4edbfccb2d54946cdd733e94d38
Pat <patrycja.filipowicz46@gmail.com>
2023-02-28 10:50:09 +0100

    New repository added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>New app!!!</h1>
+      <p>This is brans new application for your personal needs.</p>
+      <div>
+        <h3>What is Lorem Ipsum?</h3>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      </div>
+      <div>
+        <h3>Why do we use it?</h3>
+        <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
