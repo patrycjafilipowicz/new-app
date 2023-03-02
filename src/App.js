094a8e0a48558fc8ec98d1f8d328311f72f0c6bc
Pat <patrycja.filipowicz46@gmail.com>
2023-03-02 10:15:28 +0100

    new line added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ function App() {
   return (
     <div className="App">
       <h1>New app!!!</h1>
-      <p>This is brans new application for your personal needs.</p>
+      <p>This is brand new application for your personal needs.</p>
+      <p>This is brand new application for your personal needs.</p>
       <div>
         <h3>What is Lorem Ipsum?</h3>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
       </div>
       <div>
         <h3>Why do we use it?</h3>
+        <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
         <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
       </div>
     </div>
